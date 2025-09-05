11862ddb73e0d41d7f0231b1b0e24ec651d95564
opilane <nikita.orlenko234@gmai.com>
2025-09-05 11:42:23 +0300

    Tund 2

--- Nikita_Ckeel/OsalFunktsioonid.cs
+++ Nikita_Ckeel/OsalFunktsioonid.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,5 +15,107 @@ namespace Nikita_Ckeel
             float k = arv1 * arv2;
             return k;
         }
+        public static string Kuu_Nimetus(int kuu_nr)
+        {
+            string kuu = "";
+            switch (kuu_nr)
+            {
+                case 1: kuu = "Jaanuar"; break;
+                case 2: kuu = "Veebruar"; break;
+                case 3: kuu = "Märts"; break;
+                case 4: kuu = "Aprill"; break;
+                case 5: kuu = "Mai"; break;
+                case 6: kuu = "Juuni"; break;
+                case 7: kuu = "Juuli"; break;
+                case 8: kuu = "August"; break;
+                case 9: kuu = "September"; break;
+                case 10: kuu = "Octoober"; break;
+                case 11: kuu = "November"; break;
+                case 12: kuu = "December"; break;
+                default:
+                    kuu = "???";
+                    break;
+            }
+            return kuu;
+        }
+        public static string Hooaeg(int kuu_nr)
+        {
+            string hoo = "";
+
+            // Возвращаем "Talv", если месяц декабрь, январь или февраль
+            if (kuu_nr == 1 || kuu_nr == 2 || kuu_nr == 12)
+            {
+                hoo = "Talv"; // Зима
+            }
+            else if (kuu_nr > 3 && kuu_nr < 5)
+            {
+                hoo = "Kevad";
+            }
+            else if (kuu_nr > 6 && kuu_nr < 8)
+            {
+                hoo = "Suvi";
+            }
+            else if (kuu_nr > 9 && kuu_nr < 11)
+            {
+                hoo = "Sügis";
+            }
+            return hoo;
+        }
+
+        public static string juku(string vastus, int vanus)
+        {
+            string hind = "";
+            if (vanus < 6)
+            {
+                hind = "tasuta";
+            }
+            else if (vanus > 5 && vanus < 15)
+            {
+                hind = "lastepilet";
+            }
+            else if (vanus > 14 && vanus < 66)
+            {
+                hind = "täispilet";
+            }
+            else if (vanus > 65)
+            {
+                hind = "sooduspilet";
+            }
+            else if (vanus < 0 || vanus > 100)
+            {
+                hind = "viga andmetega";
+            }
+            return hind;
+
+
+        }
+
+        public static string pingi(int a)
+        {
+            string tulemus = "";
+            if (a == 0)
+            {
+                tulemus = "Te olete pinginaabrid";
+            }
+            else if (a == 1)
+            {
+                tulemus = "Te ei ole pinginaabrid(((";
+            }
+            return tulemus;
+        }
+
+        public static float Plochad(float Dlina, float Shirina)
+        {
+            if (Dlina <= 0 || Shirina <= 0)
+            {
+                Console.WriteLine("Длина и ширина должны быть положительными!");
+                return 0;
+            }
+
+            float S = Dlina * Shirina;
+            return S;
+        }
+
     }
 }
+    
--- Nikita_Ckeel/StartClass.cs
+++ Nikita_Ckeel/StartClass.cs
@@ -1,41 +1,159 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Nikita_Ckeel
 {
+
     internal class StartClass
     {
         public static void Main(string[] args)
         {
-            // Установка цвета фона и текста консоли в зеленый
-            Console.BackgroundColor = ConsoleColor.Black;
-            Console.ForegroundColor = ConsoleColor.Green;
+            string jah = "";
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Укажите длину стен:");
+            float Dlina = float.Parse(Console.ReadLine());
+            Console.WriteLine("Укажите ширину стен:");
+            float Shirina = float.Parse(Console.ReadLine());
 
-            Console.WriteLine("Tere tulemast! Mis on sinu nimi"); // Вывод приветственного сообщения и запрос имени пользователя
-            string tekst = Console.ReadLine();  // Чтение введенного имени
-            Console.WriteLine($"{tekst}, Rõõm näha!"); // Приветствие пользователя по имени
-            // Объявление целочисленной переменной a и символа taht
-            int a = 1000;
-            char taht = 'A';
-            Console.WriteLine($"Esimine arv on {a}, Sisesta b= ....."); // Вывод значения переменной a и запрос ввода числа b
-            int b = int.Parse(Console.ReadLine());  // Чтение и преобразование введенного числа в int
-            // Вывод суммы чисел a и b с использованием форматированной строки
-            Console.WriteLine("Esimine arv on {0}, Sisesta {1}. Summa on {2}", a, b, a + b);
-            double d = double.Parse(Console.ReadLine());  // Чтение и вывод числа типа double
-            Console.WriteLine(d);
-            float f = float.Parse(Console.ReadLine()); // Чтение и вывод числа типа float
-            Console.WriteLine(f);
-            bool t = true;            // Объявление булевой переменной t, присваивание true
+            float S = OsalFunktsioonid.Plochad(Dlina, Shirina);
 
-            Random rand = new Random(); //создание обьекта для генерации случайных чисел
-            a = rand.Next(1, 101); // от 1 до 101
-            Console.WriteLine(a);
+            Console.WriteLine($"Площадь пола: {S} м²");
+            Console.WriteLine("Хотите ли вы сделать реомнт?");
+            string remont = Console.ReadLine();
+            if (remont == jah.ToLower())
+            {
+                Console.WriteLine("Какая цена за квадратный метр?");
+                float hind = float.Parse(Console.ReadLine());
+                float tulemus = (S * hind);
+                Console.WriteLine($"Цена вот такая: {tulemus} евро");
+            }
+            else
+            {
+                Console.WriteLine("Ты тормози да, нормальный ответ дай!");
+            }
 
-            float vastus = OsalFunktsioonid.Kalkulator(f, a);
-            Console.WriteLine(vastus);-
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+            //Console.WriteLine("Mis sinu nimi on?");
+            //string sinuNimi = Console.ReadLine();
+            //Console.WriteLine("Mis Mis nimi on su pinginaabril?");
+            //string pingiNimi = Console.ReadLine();
+            //Random rand = new Random();
+            //int a = rand.Next(0, 2);
+            //string pinginaabrid = OsalFunktsioonid.pingi(a);
+            //Console.WriteLine(pinginaabrid);
+
+
+
+
+
+
+
+
+
+
+
+            //string juku = "juku";
+            //Console.WriteLine("Mis sinu nimi on?");
+            //string vastus = Console.ReadLine();
+            //if (vastus == juku.ToLower())
+            //{
+            //Console.WriteLine("lähme Jukuga kinno");
+            //    Console.WriteLine("Kui vana sa oled?");
+            //    int vanus = int.Parse(Console.ReadLine());
+            //    string pilet = OsalFunktsioonid.juku(vastus, vanus);        // вызываем функцию
+            //    Console.WriteLine($"Sinu pilet: {pilet}"); // выводим результат
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Sina ei ole Juku, ei saa kinno minna");
+            //}
+
+
+
+
+
+            //Random rnd = new Random();
+            ////2. Osa Valikud
+            //int kuu_number = rnd.Next(1,12);
+            //string nimetus = OsalFunktsioonid.Kuu_Nimetus(kuu_number);
+            //Console.WriteLine($"Nr: {kuu_number}-{nimetus}");
+            //Console.WriteLine("Kas tahad dekodeerida arv -> nimetusse?");
+            //string vastus = Console.ReadLine();
+            //if (vastus.ToLower() != "jah")
+            //{
+            //    Console.WriteLine("Ei taha, siis ei taha)");
+            //}
+            //else
+            //{
+            //    try
+            //    {
+            //        Console.WriteLine("Palun sisesta number: ");
+            //        kuu_number = int.Parse(Console.ReadLine());
+            //        Console.WriteLine(OsalFunktsioonid.Hooaeg(kuu_number));
+            //    }
+            //    catch (Exception error)
+            //    {
+
+            //        Console.WriteLine(error);
+            //    }
+
+            //}
+
+
+
+
+
+
+
+
+
+
+
+
+            //// Установка цвета фона и текста консоли в зеленый
+
+            //    Console.OutputEncoding = Encoding.UTF8;
+
+            //    Console.BackgroundColor = ConsoleColor.Black;
+            //    Console.ForegroundColor = ConsoleColor.Green;
+
+            //    Console.WriteLine("Tere tulemast! Mis on sinu nimi"); // Вывод приветственного сообщения и запрос имени пользователя
+            //    string tekst = Console.ReadLine();  // Чтение введенного имени
+            //    Console.WriteLine($"{tekst}, Rõõm näha!"); // Приветствие пользователя по имени
+            //    // Объявление целочисленной переменной a и символа taht
+            //    int a = 1000;
+            //    char taht = 'A';
+            //    Console.WriteLine($"Esimine arv on {a}, Sisesta b= ....."); // Вывод значения переменной a и запрос ввода числа b
+            //    int b = int.Parse(Console.ReadLine());  // Чтение и преобразование введенного числа в int
+            //    // Вывод суммы чисел a и b с использованием форматированной строки
+            //    Console.WriteLine("Esimine arv on {0}, Sisesta {1}. Summa on {2}", a, b, a + b);
+            //    double d = double.Parse(Console.ReadLine());  // Чтение и вывод числа типа double
+            //    Console.WriteLine(d);
+            //    float f = float.Parse(Console.ReadLine()); // Чтение и вывод числа типа float
+            //    Console.WriteLine(f);
+            //    bool t = true;            // Объявление булевой переменной t, присваивание true
+
+            //    Random rand = new Random(); //создание обьекта для генерации случайных чисел
+            //    a = rand.Next(1, 101); // от 1 до 101
+            //    Console.WriteLine(a);
+
+            //    float vastus = OsalFunktsioonid.Kalkulator(f, a);
+            //    Console.WriteLine(vastus);
+        
+    
